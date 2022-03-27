fd3e6e4b17066307ef9bed1d57c7a1021bff7ce5
PhinaKabemba <kabembaphina@gmail.com>
2022-03-27 21:27:17 +0200

    Moving project from local to remote

--- 02-backend/Interview/src/Mydemo.java
+++
@@ -1,8 +0,0 @@
-public class Mydemo {
-    public static void main (String[] args){
-        int salary = 25000;
-        if (salary >15000){
-            System.out.println("Congratulation");
-        }
-    }
-}
